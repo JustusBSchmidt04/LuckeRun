a612aef66b8d39c6152c68cb8a82cb3536fd36bf
Marcel Kempel <marcel.kempel@mnd.thm.de>
2025-05-22 23:12:03 +0200

    Changed Visuals and added additional sounds
    Victory sound, end screen added, functions, improved visuals

--- luckerun/Szene/Startbildschirm/main_menu.gd
+++ luckerun/Szene/Startbildschirm/main_menu.gd
@@ -3,17 +3,18 @@ func _process(delta):
 	pass
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$PlayButton.pressed.connect(_on_start_button_pressed)
 	$OptionsButton.pressed.connect(_on_options_button_pressed)
 	$QuitButton.pressed.connect(_on_quit_button_pressed)
+	$PlayButton.pressed.connect(_on_start_button_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 
 # Called when Start button is pressed
+
+	
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Szene/World1/World1.tscn")
-	
 
 	
 
--- luckerun/Szene/SzenenWechsel/next_level.gd
+++ luckerun/Szene/SzenenWechsel/next_level.gd
@@ -2,6 +2,5 @@ extends Node2D
 
 
 func _on_area_2d_body_entered(body):
-	get_tree().change_scene("res://NextScene.tscn") #jeweilige Szene einfügen
-
+	get_tree().change_scene_to_file("res://Szene/End/End.tscn")
 	pass # Replace with function body.
