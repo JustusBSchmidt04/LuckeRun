cdb875a86a4f2ec12a75c9d14a8aa4a1080001ac
Marcel Kempel <marcel.kempel@mnd.thm.de>
2025-06-02 16:50:08 +0200

    Sprite Change and Animation added
    added Jump/ falling animation and changed sprites for all existing animations

--- luckerun/Assets/Charakters/Player/Player.gd
+++ luckerun/Assets/Charakters/Player/Player.gd
@@ -9,6 +9,8 @@ extends CharacterBody2D
 @export var speed: float = 80
 const jump_force = -270
 
+var was_in_air = false  # Tracks if in air
+
 func _ready():
 	if not spawn_point:
 		spawn_point = get_node("/root/World/Respawnpoint")
@@ -17,7 +19,7 @@ func _ready():
 	if spawn_point == null:
 		push_error("spawn_point nicht gefunden")
 	if tilemap == null:
-		push_error(" tilemap nicht gefunden")
+		push_error("tilemap nicht gefunden")
 
 func _physics_process(delta):
 	if tilemap == null:
@@ -29,21 +31,41 @@ func _physics_process(delta):
 	if Input.is_action_pressed("left"):
 		velocity.x = -speed
 		$AnimatedSprite2D.flip_h = false
-		$AnimatedSprite2D.play("walk")
 	elif Input.is_action_pressed("right"):
 		velocity.x = speed
 		$AnimatedSprite2D.flip_h = true
-		$AnimatedSprite2D.play("walk")
-	else:
-		velocity.x = 0
-		$AnimatedSprite2D.play("default")
 		
+	# Jumping
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_force
 	
-		
+	# Animation Logic
+	if not is_on_floor():
+		if $AnimatedSprite2D.animation != "jump":
+			$AnimatedSprite2D.play("jump")
+		was_in_air = true
+	else:
+		if was_in_air:
+			# Just landed
+			was_in_air = false
+			if abs(velocity.x) > 0.1:
+				if $AnimatedSprite2D.animation != "walk":
+					$AnimatedSprite2D.play("walk")
+			else:
+				if $AnimatedSprite2D.animation != "default":
+					$AnimatedSprite2D.play("default")
+		else:
+			# Already on ground, decide between walk/default
+			if abs(velocity.x) > 0.1:
+				if $AnimatedSprite2D.animation != "walk":
+					$AnimatedSprite2D.play("walk")
+			else:
+				if $AnimatedSprite2D.animation != "default":
+					$AnimatedSprite2D.play("default")
+	
 	move_and_slide()
 	
+	# Death Check
 	var used_cells = tilemap.get_used_cells()
 	for cell in used_cells:
 		if tilemap.map_to_local(cell).distance_to(global_position) < 8:
@@ -51,8 +73,8 @@ func _physics_process(delta):
 			push_error("DAMNIT")
 	
 	var tile_pos: Vector2i = tilemap.local_to_map(global_position)
-	var tile_it = tilemap.get_cell_source_id( tile_pos)
-	if tile_it !=-1:
+	var tile_it = tilemap.get_cell_source_id(tile_pos)
+	if tile_it != -1:
 		push_error("HIER")
 		respawn()
 
