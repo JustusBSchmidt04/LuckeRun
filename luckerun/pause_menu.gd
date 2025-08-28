24a3dc074579f4c44e8798c419a8ecaeb57560e1
Marcel Kempel <Marcelkempel127@gmail.com>
2025-08-28 15:09:19 +0200

    BackToMain Button in Pause Menu

--- luckerun/pause_menu.gd
+++ luckerun/pause_menu.gd
@@ -10,3 +10,17 @@ func _on_resume_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _ready():
+	$Panel/VBoxContainer/BackToMain.pressed.connect(_on_BackToMain_button_pressed)
+	GlobalDeathcounter.deaths = 0
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+# Called when Start button is pressed
+
+	
+func _on_BackToMain_button_pressed():
+	get_tree().change_scene_to_file("res://Szene/Startbildschirm/main_menu_bg.tscn")
