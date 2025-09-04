ec4f3f922234c424ae6fd798d7395415660d81f3
Marcel Kempel <marcel.kempel@mnd.thm.de>
2025-09-04 19:48:18 +0200

    Changed Assets

--- luckerun/Szene/SzenenWechsel/next_level3.gd
+++ luckerun/Szene/SzenenWechsel/next_level3.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 
+
 func _on_area_2d_body_entered(body):
-	get_tree().change_scene_to_file("res://Szene/World1/World1.tscn")
+	get_tree().change_scene_to_file("res://Szene/World2/world2.tscn")
 	#get_tree().change_scene_to_file("res://Szene/End/End.tscn") for testing the Endscreen and Highscore
 	pass # Replace with function body.
