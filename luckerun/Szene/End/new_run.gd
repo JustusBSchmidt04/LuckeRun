32af759968266c9fce17012ef3c547db6d23125b
Marcel Kempel <marcel.kempel@mnd.thm.de>
2025-09-07 16:58:30 +0200

    Fixed Bug in Pause Menu

--- luckerun/Szene/End/new_run.gd
+++ luckerun/Szene/End/new_run.gd
@@ -3,4 +3,4 @@ func _ready():
 	$".".pressed.connect(_on_back_button_pressed)
 	
 func _on_back_button_pressed():
-	get_tree().change_scene_to_file("res://Szene/Startbildschirm/main_menu_bg.tscn")
+	get_tree().change_scene_to_file("res://Szene/World1/World1.tscn")
